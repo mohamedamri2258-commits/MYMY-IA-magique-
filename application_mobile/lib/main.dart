9d40a47f7a9b19215053fe49cc2a4cf404f6377b
mohamedamri2258-commits <mohamedamri2258@gmail.com>
2026-09-13 11:14:45 +0100

    Initialize Flutter app with ChatScreen

---
+++ application_mobile/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MYMY-IA Magique',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const ChatScreen(),
+    );
+  }
+}
+
